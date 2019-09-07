6fdb6414f7c89fd9f57d578f4c51b43a52b0a4ab
Laurie <laurieHardingRussell@gmail.com>
2019-09-07 11:52:08 -0600

    Created MVP backend... it's pretty minimal.

--- master/render.py
+++ master/render.py
@@ -1,5 +1,7 @@
-from flask import Flask, Blueprint
 import subprocess
+import glob
+from flask import Blueprint, jsonify
+import zipfile
 
 renderApi = Blueprint('render', __name__)
 
@@ -8,18 +10,36 @@ def initialize(frame):
     f = request.files['file']
     f.save(f.filename)
 
-@renderApi.route('/render-frame/<filename>/<int:frame>')
-def hello(filename, frame):
-    f = request.files['file']
-    f.save(filename)
+@renderApi.route('/scenes')
+def getFiles():
+    return jsonify(glob.glob("*.blend"))
 
-    successful = returnderFrame(filename)
-    if successful == 0:
-        try:
-            return send_file("test_" + frame)
-        except Exception as e:
-            abort(500, "failed to get file")
-    abort(500, "the magic stuff didn't work")
+@renderApi.route('/render-frame/<scene>/<int:start>/<int:end>')
+def renderFrames(filename, frame):
+    # TODO add support for splitting up process to slaves.
+    for i in range(start, end):
+        successful = renderFrame(scene, i)
+        # assuming no problems :)
+    return ":)"
+
+
+# TODO, will save by scene at some point
+@renderApi.route('/download/rendered')
+def getRendered(filename, frame):
+    zipFile = zipfile.ZipFile('rendered.zip', 'w', zipfile.ZIP_DEFLATED)
+    zipDirectory('rendered', zipFile)
+    zipf.close()
+    try:
+        return send_file("rendered.zip")
+    except Exception as e:
+        abort(500, "failed to get file")
+
 
 def renderFrame(name, frame):
-    return subprocess.call("blender", ["--background", name, "--render-output //test_" "--engine CYCLES", "--render-format PNG", "--use-extension 1", "--render-frame " + frame])
+    return subprocess.call("blender", ["--background", name, "--render-output //render/render" "--engine CYCLES", "--render-format PNG", "--use-extension 1", "--render-frame " + frame])
+
+def zipDirectory(path, zipFile):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            zipFile.write(os.path.join(root, file))
--- master/route.py
+++ master/route.py
@@ -4,5 +4,5 @@ from client import clientPages
 import subprocess
 
 app = Flask(__name__)
-app.register_blueprint(renderApi)
+app.register_blueprint(renderApi, url_prefix='/render')
 app.register_blueprint(clientPages)
